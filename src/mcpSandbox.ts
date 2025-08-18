5ac511b9062bb607ffabb159556fe0b3f7e64ef5
Alessio Fanelli <fanahova@gmail.com>
2025-08-18 13:32:45 -0700

    Use custom command

--- src/mcpSandbox.ts
+++ src/mcpSandbox.ts
@@ -23,7 +23,7 @@ export const startMcpSandbox = async ({
 
   console.log("Starting mcp server...");
   await sandbox.commands.run(
-      `npx -y supergateway --base-url ${url} --port 3000 --cors --stdio "${command}"`,
+      `e2b-stdio-sse --base-url ${url} --port 3000 --cors --stdio "${command}"`,
       {
         envs,
         background: true,
